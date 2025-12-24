9ef6f85e74417c7f68d823910f02094102ab460c
DuckTron <fernandesjobslk@gmail.com>
2025-12-24 19:30:00 +0000

    fix(api-gateway): Correção do problema de URLs expostas e DDD

--- api-gateway/src/main/java/com/ministorytail/gateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/com/ministorytail/gateway/config/GatewayConfig.java
@@ -8,29 +8,25 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfig {
 
-    @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route("auth-service", r -> r
-                        .path("/auth-service/**")
-                        .filters(f -> f.stripPrefix(1))
-                        .uri("lb://AUTH-SERVICE"))
+        @Bean
+        public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+                return builder.routes()
+                                .route("auth", r -> r
+                                                .path("/auth/**")
+                                                .uri("lb://AUTH-SERVICE"))
 
-                .route("profile-service", r -> r
-                        .path("/profile-service/**")
-                        .filters(f -> f.stripPrefix(1))
-                        .uri("lb://PROFILE-SERVICE"))
+                                .route("profile", r -> r
+                                                .path("/profile/**")
+                                                .uri("lb://PROFILE-SERVICE"))
 
-                .route("story-service", r -> r
-                        .path("/story-service/**")
-                        .filters(f -> f.stripPrefix(1))
-                        .uri("lb://STORY-SERVICE"))
+                                .route("stories", r -> r
+                                                .path("/stories/**")
+                                                .uri("lb://STORY-SERVICE"))
 
-                .route("notification-service", r -> r
-                        .path("/notification-service/**")
-                        .filters(f -> f.stripPrefix(1))
-                        .uri("lb://NOTIFICATION-SERVICE"))
+                                .route("notifications", r -> r
+                                                .path("/notifications/**")
+                                                .uri("lb://NOTIFICATION-SERVICE"))
 
-                .build();
-    }
+                                .build();
+        }
 }
