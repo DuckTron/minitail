2a9263ab8acce105634d6c5ac1f1db3b86cb9e0a
DuckTron <fernandesjobslk@gmail.com>
2025-12-26 16:27:25 +0000

    feat(story): implementação da estrutura de pastas e diretórios do serviço

---
+++ story-service/src/test/java/com/ministorytail/story/StoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ministorytail.story;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
